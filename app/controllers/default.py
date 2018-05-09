44da7294867accf7fdf500947ea65374cdca1d4a
ricardo.fahham <ricardo.fahham@corp.globo.com>
2018-05-09 15:15:53 -0300

    Adicionando rotas

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,9 +1,13 @@
-
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*-
 
 from app import app
 
+@app.route("/index")
 @app.route("/")
 def index():
 	return "Hello World !!!"
+
+@app.route("/test")
+def test():
+	return "Olá !!!"
